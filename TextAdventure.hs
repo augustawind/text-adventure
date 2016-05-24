88807c29d80833cebe71fc171f97df0748936acb
Dustin Rohde <xdrohdex@gmail.com>
2016-05-24 17:48:07 -0400

    Implemented StateT Game IO data model.
    Removed the manual passing around of GameState data using a StateT IO
    transformer to do it monadically while still existing in IO.
    
    Also removed the automatic newline from most functions, and wrote
    alternatives named with a trailing underscore (printWrap_, prompt_,
    etc.) to provide this functionality as a shortcut.

--- TextAdventure.hs
+++ TextAdventure.hs
@@ -1,20 +1,23 @@
 module TextAdventure where
 
 import Control.Monad (mapM_)
+import Control.Monad.State
 import Data.Char (toLower)
 import Data.List (intercalate)
 import qualified Data.Map as Map
 import System.IO
 
+-- Game data.
+-- ---------------------------------------------------------------------------
 
 data Adventure = End
                | Do Action Adventure
                | Prompt String Switch
 
-type Action = GameState -> IO ()
+type Action = StateT Game IO ()
 type Switch = Map.Map String Adventure
 
-data GameState = GameState
+data Game = Game
     { getVars :: Vars
     , getPromptChars :: String
     , getTextWidth :: Int
@@ -23,15 +26,24 @@ data GameState = GameState
 
 type Vars = Map.Map String String
 
+-- Default options.
+defaultGame = Game
+    { getVars = Map.empty
+    , getPromptChars = ">> "
+    , getTextWidth = 78
+    , getLineChar = '-'
+    }
+
 -- Example usage.
 -- ---------------------------------------------------------------------------
 
-main = run mygame myAdventure
+main :: IO ()
+main = runStateT (run myAdventure) myGame >> return ()
 
--- GameState:
+-- Game:
 
-mygame :: GameState
-mygame = defaultGameState
+myGame :: Game
+myGame = defaultGame
 
 -- Adventure:
 
@@ -41,99 +53,123 @@ myAdventure =
         Prompt "Which direction will you take?" $
             Map.fromList [("left", Do goLeft End), ("right", Do goRight End)]
 
-intro game = do
-    printLines ["You've decided to set out on an adventure."
+intro :: StateT Game IO ()
+intro = do
+    printLines_ ["You've decided to set out on an adventure."
                 ,"You've left your house and taken the path to a crossroads."]
 
-    name <- prompt game "What is your name?"
+    name <- prompt "What is your name?"
     
-    printWrap game ("Hello, " ++ name ++ "! Your adventure begins...")
+    printWrap_ ("Hello, " ++ name ++ "! Your adventure begins...")
     pause
 
-    hr game
+    hr_
 
-goLeft game = do 
-    printWrap game "You went left. You found the treasure! You win!"
+goLeft :: StateT Game IO ()
+goLeft = printWrap_ "You went left. You found the treasure! You win!"
 
-goRight game = printWrap game "You went right. A giant boar gores you."
+goRight :: StateT Game IO ()
+goRight = printWrap_ "You went right. A giant boar gores you."
 
 -- Control flow.
 -- ---------------------------------------------------------------------------
 
 -- Run an Adventure.
-run :: GameState -> Adventure -> IO ()
-run game End = printWrap game "Game over!"
-run game (Do action adventure) = action game >> run game adventure
-run game this@(Prompt msg switch) = do
+run :: Adventure -> StateT Game IO ()
+run End = printWrap "Game over!"
+run (Do action adventure) = action >> run adventure
+run this@(Prompt msg switch) = do
     let switch' = Map.mapKeys normalize switch
 
-    choice <- cmdPrompt game (Map.keys switch') msg
+    choice <- cmdPrompt_ (Map.keys switch') msg
     case Map.lookup choice switch' of
-      Nothing        -> retry (run game this)
-      Just adventure -> run game adventure
+      Nothing        -> retry (run this)
+      Just adventure -> run adventure
 
 -- Same as prompt, but also takes a list of possible choices and
 -- prints them, normalizing the input (see `normalize`).
-cmdPrompt :: GameState -> [String] -> String -> IO String
-cmdPrompt game choices msg = do putStr str
-                                choice <- getLine
-                                blankLine
-                                return $ normalize choice
+cmdPrompt :: [String] -> String -> StateT Game IO String
+cmdPrompt choices msg = do
+    game <- get
+    let str = wordWrap width msg ++ ('\n':choicesStr) ++ (' ':promptChars)
+        choicesStr = "(" ++ intercalate ", " choices ++ ")" 
+        width = getTextWidth game
+        promptChars = getPromptChars game
+    liftIO $ do
+        putStr str
+        choice <- getLine
+        return $ normalize choice
 
-   where str = wordWrap width msg ++ ('\n':choicesStr) ++ (' ':promptChars)
-         choicesStr = "(" ++ intercalate ", " choices ++ ")" 
-         width = getTextWidth game
-         promptChars = getPromptChars game
+-- @cmdPrompt@ with a blank line added to the end.
+cmdPrompt_ :: [String] -> String -> StateT Game IO String
+cmdPrompt_ cs msg = cmdPrompt cs msg >>= \x -> liftIO blankLine >> return x
 
 -- Print something then prompt for input. 
-prompt :: GameState -> String -> IO String
-prompt game message = do putStr str
-                         answer <- fmap strip getLine
-                         blankLine
-                         return answer
-    where str = wordWrap width message ++ ('\n':promptChars)
-          width = getTextWidth game
-          promptChars = getPromptChars game
+prompt :: String -> StateT Game IO String
+prompt message = do
+    game <- get
+    let str = wordWrap width message ++ ('\n':promptChars)
+        width = getTextWidth game
+        promptChars = getPromptChars game
+    liftIO $ do putStr str
+                answer <- fmap strip getLine
+                return answer
+
+-- @prompt@ with a blank line added to the end.
+prompt_ :: String -> StateT Game IO String
+prompt_ msg = prompt msg >>= \x -> liftIO blankLine >> return x
 
 
 -- Print a "try again" message and execute a given IO action.
-retry :: IO () -> IO ()
-retry action = putStrLn "Invalid input. Please try again." >> blankLine >> action
+retry :: StateT Game IO () -> StateT Game IO ()
+retry action = do
+    liftIO $ putStrLn "Invalid input. Please try again." >> blankLine
+    action 
+    return ()
 
 -- Pause execution and wait for a keypress to continue.
-pause :: IO ()
-pause = putStr "<Press any key to continue...>" >> getChar >> return ()
+pause :: StateT Game IO ()
+pause = liftIO $
+    putStr "<Press any key to continue...>" >> getChar >> return ()
 
 -- Output.
 -- ---------------------------------------------------------------------------
-
--- Default options.
-defaultGameState = GameState
-    { getVars = Map.empty
-    , getPromptChars = ">> "
-    , getTextWidth = 78
-    , getLineChar = '-'
-    }
 
 -- Print a blank line.
 blankLine :: IO ()
 blankLine = putChar '\n'
 
 -- Print a horizontal rule.
-hr :: GameState -> IO ()
-hr game = putStrLn (replicate width char) >> blankLine
-    where width = getTextWidth game
-          char = getLineChar game
+hr :: StateT Game IO ()
+hr = do
+    game <- get
+    let width = getTextWidth game
+        char = getLineChar game
+    liftIO $ putStrLn (replicate width char)
 
--- Print a list of Strings line by line.
-printLines :: [String] -> IO ()
-printLines xs = mapM_ putStrLn xs >> blankLine
+-- @hr@ with a blank line added to the end.
+hr_ :: Action
+hr_ = hr >> liftIO blankLine
+
+-- Print a list of Strings line by line, wrapping each line to the given width.
+printLines :: [String] -> StateT Game IO ()
+printLines xs = mapM_ printWrap xs
+
+-- @printLines@ with a blank line added to the end.
+printLines_ :: [String] -> StateT Game IO ()
+printLines_ xs = printLines xs >> liftIO blankLine
 
 -- Print a String, wrapping its text to the given width.
-printWrap :: GameState -> String -> IO ()
-printWrap game str = putStrLn (wordWrap (getTextWidth game) str) >> blankLine
+printWrap :: String -> StateT Game IO ()
+printWrap str = do
+    game <- get
+    liftIO $ putStrLn (wordWrap (getTextWidth game) str)
 
--- String manipulation helpers.
+-- @printWrap@ with a blank line added to the end.
+printWrap_ :: String -> Action
+printWrap_ str = printWrap str >> liftIO blankLine
+
+-- String helpers.
 -- ---------------------------------------------------------------------------
 
 -- Remove leading and trailing whitespace from a String.
