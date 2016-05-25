f67c07994f53b989273e6ae875f5f2c541c60e4d
Dustin Rohde <xdrohdex@gmail.com>
2016-05-24 22:13:18 -0400

    Added a module for Python style string formatting.

---
+++ TemplateString.hs
@@ -0,0 +1,59 @@
+{-# LANGUAGE FlexibleContexts #-}
+module TemplateString
+    ( (-%-)
+    , parseTemplate
+    ) where
+
+import Control.Monad
+import qualified Data.Map as Map
+
+import Text.ParserCombinators.Parsec
+
+-- | @parseTemplate@ as an infix operator.
+infixl 4 -%-
+(-%-) :: String -> Map.Map String String -> Either String String
+str -%- vars = parseTemplate str vars
+
+-- | Parse a template string, replacing each variable (denoted by "%(var)")
+-- with a corresponding value in the given Map. Returns 'Left err' if
+-- a name doesn't exist in the Map.
+parseTemplate :: String -> Map.Map String String -> Either String String
+parseTemplate ""  _    = Right ""
+parseTemplate str vars = foldM f "" (extractPairs str) 
+    where
+        f acc (plainText, "") = Right $ acc ++ plainText
+        f acc (plainText, var) =
+            case Map.lookup var vars of
+              Nothing -> Left $ "Name '" ++ var ++ "' doesn't exist"
+              Just name -> Right $ acc ++ plainText ++ name
+
+-- Run the @pairs@ parser on a string. OK to throw an error on failure since
+-- the @pairs@ parser always succeeds.
+extractPairs :: String -> [(String, String)]
+extractPairs str = either (error . show) id $ parse pairs source str
+    where source = "<template string>: " ++ str
+
+-- Parse each @pair@ of (plain text, template var) into a list of 2-tuples.
+pairs :: Parser [(String, String)]
+pairs = pair `manyTill` eof
+
+-- Parse a @templateVar@, and return the text leading it up to it and the
+-- @templateVar@ with its template formatting stripped, in a 2-tuple.
+pair :: Parser (String, String)
+pair = try ((,) <$> anythingBut templateVar <*> try templateVar) <|>
+           ((,) <$> many anyChar <*> string "")
+
+-- Parse a template variable, in the format "%(myTemplateVar)". Returns
+-- the text between the parentheses.
+templateVar :: Parser String
+templateVar = char '%' *> anyBetweenChars '(' ')'
+
+-- Parse anything that is not matched by the given parser. Boolean @not@
+-- for parsers.
+anythingBut :: Parser String -> Parser String
+anythingBut p = anyChar `manyTill` (lookAhead (try p)) <|> many anyChar
+
+-- Match anything between Chars @open@ and @close@, but not nothing.
+anyBetweenChars :: Char -> Char -> Parser String
+anyBetweenChars open close =
+    between (char open) (char close) $ many1 $ noneOf [close]
