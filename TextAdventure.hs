abfc3a10cbbde093aeaf0d06d1fe024b2459cb08
Dustin Rohde <xdrohdex@gmail.com>
2016-05-30 19:11:51 -0400

    Edited wordWrap to split on words intelligently.

--- TextAdventure.hs
+++ TextAdventure.hs
@@ -231,9 +231,23 @@ blankLine = putChar '\n'
 wordWrap :: Int -> String -> String
 wordWrap width str
   | length str' <= width = str'
-  | otherwise = take width str' ++ "\n" ++ wordWrap width (drop width str')
+  | otherwise = firstLine ++ "\n" ++ wordWrap width rest
   where str' = filter notLineBreak str
         notLineBreak = not . (`elem` "\n\r")
+        (firstLine, rest) = smartLineBreak width str'
+
+-- Split a String at the given width (at most), or at the closest word
+-- before that width. Words that are longer than the given width will be split.
+smartLineBreak :: Int -> String -> (String, String)
+smartLineBreak width str = (first, rest)
+    where 
+        first =
+            takeWhile (not . isWhitespace) $ 
+                case (reverse . dropWhile (not . isWhitespace) . reverse) chunk of
+                    ""   -> chunk
+                    str' -> str'
+        rest = dropWhile isWhitespace . drop (length first) $ str
+        chunk = take width str
 
 -- Strip leading and trailing whitespace and make everything lowercase.
 normalize :: String -> String
@@ -242,4 +256,6 @@ normalize = map toLower . strip
 -- Remove leading and trailing whitespace from a String.
 strip :: String -> String
 strip = reverse . dropWhile isWhitespace . reverse . dropWhile isWhitespace
-    where isWhitespace = (`elem` [' ', '\t', '\n', '\r'])
+
+isWhitespace :: Char -> Bool
+isWhitespace = (`elem` [' ', '\t', '\n', '\r'])
