36ced39f5e2948c653785e4c6272cf41e57301d2
Dustin Rohde <xdrohdex@gmail.com>
2016-05-30 17:50:01 -0400

    Expanded demo Adventure.

--- demo.hs
+++ demo.hs
@@ -18,14 +18,47 @@ myAdventure =
                    ,Print "Which direction will you go?"
                    ])
          $dispatcher$
-             [("left",
-                 Node (Print "You went left. You found the treasure! You win!")
-                      gameOver)
-             ,("right",
-                 Node (Print "You went right. A giant boar gores you. You die.")
-                      gameOver)
+             [("left", wentLeft)
+             ,("right", wentRight)
              ]
 
 intro :: [String]
 intro = ["You've decided to set out on an adventure."
         ,"You've left your house and taken the path to a crossroads."]
+
+wentLeft :: Adventure
+wentLeft = Node (PrintLines ["You went left."
+                            ,"You see a strange object."
+                            ,"Do you pick it up?"])
+                $dispatcher$
+                    [("yes", Node (Print ("You pick it up and instantly " ++
+                        "attain enlightenment. You win!"))
+                        gameOver
+                     )
+                    ,("no", Node (Print ("Your skepticism precedes you. " ++
+                        "You turn to leave the object behind " ++
+                        "and you see a treasure chest apparate out of thin " ++
+                        "air. Do you wish to open it?"))
+                        $dispatcher$
+                            [("yes", openedChest)
+                            ,("no", didntOpenChest)
+                            ]
+                     )
+                    ]
+
+wentRight :: Adventure
+wentRight = Node (PrintLines ["You went right."
+                             ,"A giant boar gores you."
+                             ,"You lose!"
+                             ])
+                 gameOver
+
+openedChest :: Adventure
+openedChest = Node (Print "You found the treasure! You win!") gameOver
+
+didntOpenChest :: Adventure
+didntOpenChest = Node (PrintLines ["You have denied too many opportunities!"
+                                  ,"You are being punished for your ungratefulness."
+                                  ,"You lose!"
+                                  ])
+                      gameOver
