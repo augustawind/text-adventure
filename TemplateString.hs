6817368aa2411cf955ea51b15d88a7289f8c8386
Dustin Rohde <xdrohdex@gmail.com>
2016-05-31 15:10:15 -0400

    Refactored TemplateString module.

--- TemplateString.hs
+++ TemplateString.hs
@@ -42,7 +42,7 @@ pairs = pair `manyTill` eof
 -- Parse a @templateVar@, and return the text leading it up to it and the
 -- @templateVar@ with its template formatting stripped, in a 2-tuple.
 pair :: Parser (String, String)
-pair = try ((,) <$> anythingBut templateVar <*> try templateVar) <|>
+pair = try ((,) <$> anythingBut templateVar <*> templateVar) <|>
            ((,) <$> many anyChar <*> string "")
 
 -- Parse a template variable, in the format "%(myTemplateVar)". Returns
@@ -53,7 +53,7 @@ templateVar = char '%' *> anyBetweenChars '(' ')'
 -- Parse anything that is not matched by the given parser. Boolean @not@
 -- for parsers.
 anythingBut :: Parser String -> Parser String
-anythingBut p = anyChar `manyTill` (lookAhead (try p)) <|> many anyChar
+anythingBut p = try (anyChar `manyTill` p) <|> many anyChar
 
 -- Match anything between Chars @open@ and @close@, but not nothing.
 anyBetweenChars :: Char -> Char -> Parser String
