53e84e4545213aaec77bc51da7b26a70ee421081
Dustin Rohde <xdrohdex@gmail.com>
2016-05-30 20:52:28 -0400

    Flattened the demo adventure.

--- demo.hs
+++ demo.hs
@@ -15,51 +15,40 @@ myAdventure =
          ,Print "Hello, %(name)! Your adventure begins..."
          ,Pause
          ,HR
-         ,Print "Which direction will you go?"
-         ]
-         $dispatcher$
-             [("left", wentLeft)
-             ,("right", wentRight)
-             ]
-
-intro :: [String]
-intro = ["You've decided to set out on an adventure."
-        ,"You've left your house and taken the path to a crossroads."]
-
-wentLeft :: Adventure
-wentLeft = Node [PrintLines ["You went left."
+         ,Print "Which direction will you go?"]
+    $dispatcher$
+        [("left",
+           Node [PrintLines ["You went left."
                             ,"You see a strange object."
                             ,"Do you pick it up?"]]
-                $dispatcher$
-                    [("yes", Node [Print ("You pick it up and instantly " ++
-                        "attain enlightenment. You win!")]
-                        gameOver
-                     )
-                    ,("no", Node [Print ("Your skepticism precedes you. " ++
-                        "You turn to leave the object behind " ++
-                        "and you see a treasure chest apparate out of thin " ++
-                        "air. Do you wish to open it?")]
-                        $dispatcher$
-                            [("yes", openedChest)
-                            ,("no", didntOpenChest)
-                            ]
-                     )
-                    ]
+           $dispatcher$
+               [("yes",
+                   Node [Print ("You pick it up and instantly " ++
+                                "attain enlightenment. You win!")]
+                   gameOver
+                )
+               ,("no",
+                   Node [Print ("Your skepticism precedes you. " ++
+                                "You turn to leave the object behind " ++
+                                "and you see a treasure chest apparate out of thin " ++
+                                "air. Do you wish to open it?")]
+                   $dispatcher$
+                       [("yes",
+                           Node [Print "You found the treasure! You win!"] gameOver)
 
-wentRight :: Adventure
-wentRight = Node [Print "You went right."
-                 ,Prompt "lastWords" ["help", "fuck"] "Any last words?"
-                 ,PrintLines ["A giant boar gores you."
-                             ,"Before you die, you say \"%(lastWords)\"."
-                             ,"You lose!"]]
-                 gameOver
-
-openedChest :: Adventure
-openedChest = Node [Print "You found the treasure! You win!"] gameOver
-
-didntOpenChest :: Adventure
-didntOpenChest = Node [PrintLines ["You have denied too many opportunities!"
-                                  ,"You are being punished for your ungratefulness."
-                                  ,"You lose!"
-                                  ]]
-                      gameOver
+                       ,("no",
+                           Node [PrintLines ["You have denied too many opportunities!"
+                                            ,"You are being punished for your ungratefulness."
+                                            ,"You lose!"]]
+                           gameOver
+                        )]
+                )]
+         )
+        ,("right",
+           Node [Print "You went right."
+                ,Prompt "lastWords" ["help", "fuck"] "Any last words?"
+                ,PrintLines ["A giant boar gores you."
+                            ,"Before you die, you say \"%(lastWords)\"."
+                            ,"You lose!"]]
+           gameOver
+         )]
