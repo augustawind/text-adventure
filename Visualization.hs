22ce93de819cba7df7e9e2669e362302a5c810f8
Dustin Rohde <xdrohdex@gmail.com>
2016-05-31 19:07:48 -0400

    Fixed module header.

--- Visualization.hs
+++ Visualization.hs
@@ -1,10 +1,11 @@
-module Visualization where
+module Visualization 
+    ( visualize,
+    , visualizeVerbose
+    ) where
 
-import Control.Monad (mapM_)
 import Data.List (intercalate)
 import qualified Data.Map as Map
 
-import TextAdventure
 import TextAdventure (Adventure(..), Output(..), Nexus(..))
 import StringUtils (wordWrap)
 
@@ -64,51 +65,3 @@ formatOutput indentLevel =
         wrapLines = intercalate "\n" . map wrapIndented . map show
         wrapIndented = wordWrap (78 - indentLevel)
         indentStr = replicate indentLevel ' '
-
--- Custom game over.
-gameOver :: Nexus
-gameOver = EndGame "Game over!"
-
--- Adventure:
-myAdventure :: Adventure
-myAdventure =
-    Node [PrintLines ["You've decided to set out on an adventure."
-                     ,"You've left your house and taken the path to a crossroads."]
-         ,Prompt "name" [] "What is your name?"
-         ,Print "Hello, %(name)! Your adventure begins..."
-         ,Pause
-         ,HR]
-    $ ask "Which direction will you go?" $
-        [("left",
-           Node [PrintLines ["You went left."
-                            ,"You see a strange object."]]
-           $ ask "Do you pick it up?" $
-               [("yes",
-                   Node [Print ("You pick it up and instantly " ++
-                                "attain enlightenment. You win!")]
-                   gameOver
-                )
-               ,("no",
-                   Node [Print ("Your skepticism precedes you. " ++
-                                "You turn to leave the object behind " ++
-                                "and you see a treasure chest apparate out of thin air.")]
-                   $ ask "Do you wish to open it?" $
-                       [("yes",
-                           Node [Print "You found the treasure! You win!"] gameOver)
-
-                       ,("no",
-                           Node [PrintLines ["You have denied too many opportunities!"
-                                            ,"You are being punished for your ungratefulness."
-                                            ,"You lose!"]]
-                           gameOver
-                        )]
-                )]
-         )
-        ,("right",
-           Node [Print "You went right."
-                ,Prompt "lastWords" ["help", "fuck"] "Any last words?"
-                ,PrintLines ["A giant boar gores you."
-                            ,"Before you die, you say \"%(lastWords)\"."
-                            ,"You lose!"]]
-           gameOver
-         )]
